f0e8f8b4058e41539d1db4497b599b777d92cdce
C-R4Y0-N <130323486+C-R4Y0-N@users.noreply.github.com>
2023-06-28 21:40:12 -0600

    28062023

---
+++ forloops.py
@@ -0,0 +1,10 @@
+import time
+#  for loop = a statement that will execute it's block of code a limit amount of times
+#   while loop = unlimited
+#   for = limited
+
+#for i in range (10):
+#    print(i+1)
+
+for i in range (50,100+1):
+    print (i)
---
+++ whileloops.py
@@ -0,0 +1,8 @@
+#while loop =a statement that will execute it's block of code, as long i'ts condition remains true 
+#while 1==1:
+#    print("Help I'm stuck in a infinite loop")
+
+#name = None
+#while not name:
+#    name = input("Which is your name?")
+#print ("Hello",name)
